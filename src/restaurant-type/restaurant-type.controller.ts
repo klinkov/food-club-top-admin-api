5d9523137a30fd92d062a347218e93b1ddc84b1b
Anton <anton.stupnikov97@gmail.com>
2021-01-20 13:50:17 +0300

    fix place route

--- src/restaurant-type/restaurant-type.controller.ts
+++ src/restaurant-type/restaurant-type.controller.ts
@@ -3,19 +3,19 @@ import { CoreGatewayService } from 'src/core-gateway/core-gateway.service';
 import { RestaurantTypeDto } from './dto/RestaurantTypeDto';
 import { IDeleteResponse } from 'src/core/interfaces/IDeleteResponse';
 
-@Controller('restaurant-type')
+@Controller('place-type')
 export class RestaurantTypeController {
     constructor(private coreGateway: CoreGatewayService) { }
 
     @Post()
     async create(@Body() body: RestaurantTypeDto) {
-        const createdType = (await this.coreGateway.request('restaurant-type-create', body)).toClass(RestaurantTypeDto);
+        const createdType = (await this.coreGateway.request('place-type-create', body)).toClass(RestaurantTypeDto);
         return createdType;
     }
 
     @Put(':id')
     async edit(@Param('id', ParseIntPipe) id: number, @Body() body: RestaurantTypeDto) {
-        const editedType = (await this.coreGateway.request<RestaurantTypeDto>('restaurant-type-edit', {
+        const editedType = (await this.coreGateway.request<RestaurantTypeDto>('place-type-edit', {
             id,
             ...body
         })).toClass(RestaurantTypeDto);
@@ -25,7 +25,7 @@ export class RestaurantTypeController {
     @Get()
     async list() {
         const list = (await this.coreGateway
-            .request<RestaurantTypeDto[]>('restaurant-type-list',))
+            .request<RestaurantTypeDto[]>('place-type-list',))
             .toClassList(RestaurantTypeDto);
         return list;
     }
@@ -33,14 +33,14 @@ export class RestaurantTypeController {
     @Get(':id')
     async getOne(@Param('id', ParseIntPipe) id: number): Promise<RestaurantTypeDto> {
         const item = (await this.coreGateway
-            .request<RestaurantTypeDto>('restaurant-type-one', { id }))
+            .request<RestaurantTypeDto>('place-type-one', { id }))
             .toClass(RestaurantTypeDto);
         return item;
     }
 
     @Delete(':id')
     async delete(@Param('id', ParseIntPipe) id: number): Promise<IDeleteResponse> {
-        await this.coreGateway.request('restaurant-type-delete', {
+        await this.coreGateway.request('place-type-delete', {
             id,
         });
         return {
